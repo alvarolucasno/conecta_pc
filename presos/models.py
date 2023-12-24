3b09634176ac24a0451a035607d2ea173302dbd5
Alvaro <alvarolucasno@gmail.com>
2023-12-24 12:04:24 -0300

    funcionando

--- presos/models.py
+++ presos/models.py
@@ -1,3 +1,55 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+def format_date_for_path(date):
+    return date.strftime('%Y%m%d')
+
+def upload_to_perfil_esquerdo(instance, filename):
+    data_formatada = format_date_for_path(instance.data_fotos)
+    return f'presos/{slugify(instance.nome_completo)}_{slugify(instance.mae)}/{slugify(instance.nome_completo)}_{data_formatada}_perfil_esquerdo.jpg'
+
+def upload_to_frontal(instance, filename):
+    data_formatada = format_date_for_path(instance.data_fotos)
+    return f'presos/{slugify(instance.nome_completo)}_{slugify(instance.mae)}/{slugify(instance.nome_completo)}_{data_formatada}_face.jpg'
+
+def upload_to_perfil_direito(instance, filename):
+    data_formatada = format_date_for_path(instance.data_fotos)
+    return f'presos/{slugify(instance.nome_completo)}_{slugify(instance.mae)}/{slugify(instance.nome_completo)}_{data_formatada}_perfil_direito.jpg'
+
+
+class Preso(models.Model):
+    # Fotos
+    perfil_esquerdo = models.ImageField(upload_to=upload_to_perfil_esquerdo)
+    frontal = models.ImageField(upload_to=upload_to_frontal)
+    perfil_direito = models.ImageField(upload_to=upload_to_perfil_direito)
+    
+    # Datas e strings
+    data_fotos = models.DateField()
+    origem_fotos = models.CharField(max_length=255)
+    nome_completo = models.CharField(max_length=255)
+    data_nascimento = models.DateField()
+    mae = models.CharField(max_length=255)
+    pai = models.CharField(max_length=255, blank=True, null=True)
+
+    # Documentos
+    rg = models.CharField(max_length=20, blank=True, null=True)
+    uf_rg = models.CharField(max_length=100, blank=True, null=True)
+    cpf = models.CharField(max_length=14, blank=True, null=True)
+    cnh = models.CharField(max_length=20, blank=True, null=True)
+    nis = models.CharField(max_length=20, blank=True, null=True)
+    sap = models.CharField(max_length=10, blank=True, null=True)
+
+    # Detalhes da prisão
+    razao_prisao = models.CharField(max_length=255)
+    numero_procedimento = models.CharField(max_length=50)
+    observacao = models.TextField(blank=True, null=True)
+
+    # Campos de controle
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome_completo
+
--- presos/views.py
+++ presos/views.py
@@ -9,14 +9,68 @@ from io import BytesIO
 from datetime import datetime
 from django.contrib import messages
 import time
+from .models import Preso
+from django.utils.text import slugify
+import base64
+from django.core.files.base import ContentFile
+
 
 @login_required(login_url='/login/')
 def cadastrar(request):
+    if request.method == 'POST':
+                
+        data_fotos = request.POST.get('data_fotos')
+        origem_fotos = request.POST.get('origem_fotos')
+        nome_completo = request.POST.get('nome_completo')
+        data_nascimento = request.POST.get('data_nascimento')
+        mae = request.POST.get('mae')
+        pai = request.POST.get('pai')
+        rg = request.POST.get('rg')
+        uf_rg = request.POST.get('uf_rg')
+        cpf = request.POST.get('cpf')
+        cnh = request.POST.get('cnh')
+        nis = request.POST.get('nis')
+        sap = request.POST.get('sap')
+        razao_prisao = request.POST.get('razao_prisao')
+        numero_procedimento = request.POST.get('numero_procedimento')
+        observacao = request.POST.get('observacao')
+        
+        dicionario = {
+            'data_fotos': data_fotos,
+            'origem_fotos': origem_fotos,
+            'nome_completo': nome_completo,
+            'data_nascimento': data_nascimento,
+            'mae': mae,
+            'pai': pai,
+            'rg': rg,
+            'uf_rg': uf_rg,
+            'cpf': cpf,
+            'cnh': cnh,
+            'nis': nis,
+            'sap': sap,
+            'razao_prisao': razao_prisao,
+            'numero_procedimento': numero_procedimento,
+            'observacao': observacao,
+        }
+        
+        for i in range(1, 4):  # Para cada uma das 3 imagens
+            image_data = request.POST.get(f'croppedImage{i}')
+            if image_data:
+                format, imgstr = image_data.split(';base64,') 
+                ext = format.split('/')[-1] 
+                image = ContentFile(base64.b64decode(imgstr), name=f'temp.{ext}')
+
+                file_path = f'media/teste/{nome_completo}_{i}.{ext}'
+                with open(file_path, 'wb+') as destination:
+                    for chunk in image.chunks():
+                        destination.write(chunk)
+
+        return HttpResponse(dicionario)
     
     nome = request.user.first_name
     sobrenome = request.user.last_name
     foto = request.user.foto
-    
+
     ufs_unicos = Cidade.objects.values_list('uf', flat=True).distinct()
     ufs_unicos = sorted(ufs_unicos)
 
